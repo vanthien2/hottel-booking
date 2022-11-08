9f8e4301e9879b0c30debb76c03b61183bd59859
thien le <levanthien0811@gmail.com>
2022-11-08 14:12:44 +0700

    len

--- hottel booking/Controllers/ViewHomeController.cs
+++ hottel booking/Controllers/ViewHomeController.cs
@@ -21,6 +21,41 @@ namespace hottel_booking.Controllers
             ViewBag.TrangthaiThanhToan = new SelectList(data.TrangthaiThanhToans, "mathanhtoan", "trangthai");
             return View();
         }
+            [HttpPost]
+
+            public ActionResult Home([Bind(Include = "madatPhong,hoten,diachi,email,dienthoai,ngaydatPhong,ngaytraPhong,hinhthucthanhtoan,loaiPhong,succhua,total_amount")] tbl_booking tbl_booking)
+            {
+                int numberofday = Convert.ToInt32((tbl_booking.ngaydatPhong - tbl_booking.booking_from).TotalDays);
+
+                tbl_room_type objroom = db.tbl_room_type.Single(model => model.room_type_id == tbl_booking.assigned_room);
+                decimal Roomprices = Convert.ToDecimal(objroom.room_price);
+                decimal TotalAmount = Roomprices * numberofday;
+                tbl_booking roombooking = new tbl_booking()
+                {
+                    customer_name = tbl_booking.customer_name,
+                    customer_address = tbl_booking.customer_address,
+                    customer_email = tbl_booking.customer_email,
+                    customer_phone_no = tbl_booking.customer_phone_no,
+                    booking_from = tbl_booking.booking_from,
+                    booking_to = tbl_booking.booking_to,
+                    payment_type = tbl_booking.payment_type,
+                    assigned_room = tbl_booking.assigned_room,
+                    no_of_members = tbl_booking.no_of_members,
+                    total_amount = TotalAmount,
+                };
+
+
+                if (ModelState.IsValid)
+                {
+                    db.tbl_booking.Add(roombooking);
+                    db.SaveChanges();
+                    return RedirectToAction("Home");
+                }
+
+                ViewBag.assigned_room = new SelectList(db.tbl_room_type, "room_type_id", "room_name", tbl_booking.assigned_room);
+                ViewBag.payment_type = new SelectList(db.tbl_payment_type, "payment_type_id", "payment_type", tbl_booking.payment_type);
+                return View(tbl_booking);
+            }
         public ActionResult Room()
         {
 
---
+++ hottel booking/Controllers/danhsachPhongController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using hottel_booking.Models;
+
+
+namespace hottel_booking.Controllers
+{
+    public class danhsachPhongController : Controller
+    {
+        // GET: danhsachPhong
+        DataDataContext data = new DataDataContext();
+        public ActionResult ListPhong()
+        {
+            var all_Phong = from ss in data.Phongs select ss;
+
+            return View(all_Phong);
+        }
+    }
+}
